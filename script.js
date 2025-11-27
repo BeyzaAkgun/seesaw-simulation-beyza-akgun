530f982f6865de8b636b8ff57921a777f3b9a5f0
BeyzaAkgun <beyzaakgun@hotmail.com>
2025-11-27 19:33:18 +0300

    step3: implement torque calculation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let objects = [];
+
 const clickArea = document.querySelector(".click-area");
 const plank = document.querySelector(".plank");
 
@@ -35,7 +37,47 @@ clickArea.addEventListener("click", (event) => {
     object.style.left = (normalizedX + 200 - size / 2) + 'px';
     object.style.top = (-size / 2) + 'px';
 
+    //Storing the object data
+    const newObject = {
+        element: object,
+        weight: weight,
+        distance: normalizedX
+    };
+
+    objects.push(newObject);
+    
+    calculateTorque();
+
     console.log(`Object created: ${weight}kg at ${normalizedX}px from center`);
 
 
 });
+
+// Function to calculate torque and weights on both sides
+function calculateTorque() {
+    let leftTorque = 0;
+    let rightTorque = 0;
+    let leftWeight = 0;
+    let rightWeight = 0;
+    
+    objects.forEach(obj => {
+        if (obj.distance < 0) {
+            // Left side
+            leftTorque += obj.weight * Math.abs(obj.distance);
+            leftWeight += obj.weight;
+        } else if (obj.distance > 0) {
+            //Right side
+            rightTorque += obj.weight * obj.distance;
+            rightWeight += obj.weight;
+        }
+    });
+    
+    const netTorque = rightTorque - leftTorque;
+    
+    console.log(" TORQUE CALCULATION RESULTS ");
+    console.log("Left Side - Weight:", leftWeight + "kg", "Torque:", leftTorque);
+    console.log("Right Side - Weight:", rightWeight + "kg", "Torque:", rightTorque);
+    console.log("Net Torque:", netTorque);
+    console.log("Total Object Count:", objects.length);
+    return { netTorque, leftWeight, rightWeight };
+}
